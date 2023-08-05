2335001721286de1e97996361932b4e607ed5e17
Ritsuki Goto <develop@mizucoffee.net>
2023-08-05 18:30:53 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/configration/GlobalInterceptorConfiguration.kt
+++ src/main/kotlin/wedding/kanshasai/backend/configration/GlobalInterceptorConfiguration.kt
@@ -4,9 +4,8 @@ import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor
 import org.springframework.context.annotation.Configuration
 import wedding.kanshasai.backend.configration.interceptor.GrpcLogInterceptor
 
-
 @Configuration(proxyBeanMethods = false)
-class GlobalInterceptor {
+class GlobalInterceptorConfiguration {
     @GrpcGlobalServerInterceptor
     fun logServerInterceptor() = GrpcLogInterceptor()
 }
