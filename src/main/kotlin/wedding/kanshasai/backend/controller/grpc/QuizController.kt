bb8f44fe64e9d69cc7434c6160793107a89b7e84
Ritsuki Goto <develop@mizucoffee.net>
2023-08-05 18:32:00 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/controller/grpc/QuizController.kt
+++ src/main/kotlin/wedding/kanshasai/backend/controller/grpc/QuizController.kt
@@ -1,9 +1,7 @@
 package wedding.kanshasai.backend.controller.grpc
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.grpc.Context
 import net.devh.boot.grpc.server.service.GrpcService
-import org.slf4j.MDC
 import wedding.kanshasai.v1.*
 import wedding.kanshasai.v1.QuizServiceGrpcKt.QuizServiceCoroutineImplBase
 
