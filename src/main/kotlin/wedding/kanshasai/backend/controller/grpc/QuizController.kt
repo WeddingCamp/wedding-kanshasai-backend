6040bded6615a8907abd10f7c72d55840ac928f9
Ritsuki Goto <develop@mizucoffee.net>
2023-08-15 12:43:01 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/controller/grpc/QuizController.kt
+++ src/main/kotlin/wedding/kanshasai/backend/controller/grpc/QuizController.kt
@@ -3,11 +3,30 @@ package wedding.kanshasai.backend.controller.grpc
 import net.devh.boot.grpc.server.service.GrpcService
 import wedding.kanshasai.v1.*
 import wedding.kanshasai.v1.QuizServiceGrpcKt.QuizServiceCoroutineImplBase
+import java.util.UUID
 
 @GrpcService
 class QuizController : QuizServiceCoroutineImplBase() {
     override suspend fun listQuizzes(request: ListQuizzesRequest): ListQuizzesResponse {
-        TODO("NOT IMPLEMENTED")
+        return ListQuizzesResponse.newBuilder().run {
+            addAllQuizzes(listOf(
+                ListQuizzesResponse.Quiz.newBuilder().run {
+                    quizId = UUID.randomUUID().toString()
+                    body = "新婦婦の実家の最寄駅は？"
+                    addAllChoices(listOf("日根野駅","泉佐野駅","熊取駅","鳳駅"))
+                    correctAnswer = "{\"answer_id\": 1}"
+                    build()
+                },
+                ListQuizzesResponse.Quiz.newBuilder().run {
+                    quizId = UUID.randomUUID().toString()
+                    body = "最寄りのスーパーはどこ？"
+                    addAllChoices(listOf("ライフ","イオン","イズミヤ","イトーヨーカドー"))
+                    correctAnswer = "{\"answer_id\": 1}"
+                    build()
+                }
+            ))
+            build()
+        }
     }
 
     override suspend fun createQuiz(request: CreateQuizRequest): CreateQuizResponse {
--- src/main/kotlin/wedding/kanshasai/backend/controller/grpc/ScreenController.kt
+++ src/main/kotlin/wedding/kanshasai/backend/controller/grpc/ScreenController.kt
@@ -1,9 +1,15 @@
 package wedding.kanshasai.backend.controller.grpc
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import net.devh.boot.grpc.server.service.GrpcService
 import wedding.kanshasai.v1.*
 import wedding.kanshasai.v1.ScreenServiceGrpcKt.ScreenServiceCoroutineImplBase
+import java.util.*
+
+private val logger = KotlinLogging.logger {}
 
 @GrpcService
 class ScreenController : ScreenServiceCoroutineImplBase() {
@@ -11,7 +17,20 @@ class ScreenController : ScreenServiceCoroutineImplBase() {
         TODO("NOT IMPLEMENTED")
     }
 
-    override fun streamScreenEvent(request: StreamScreenEventRequest): Flow<StreamScreenEventResponse> {
-        TODO("NOT IMPLEMENTED")
+    override fun streamScreenEvent(request: StreamScreenEventRequest): Flow<StreamScreenEventResponse> = flow {
+        while (true) {
+            delay(1000)
+            emit(StreamScreenEventResponse.newBuilder().run {
+                eventType = ScreenEventType.SCREEN_EVENT_TYPE_QUIZ
+                quizEvent = StreamScreenEventResponse.QuizEvent.newBuilder().run {
+                    quizId = UUID.randomUUID().toString()
+                    body = "新婦婦の実家の最寄駅は？"
+                    addAllChoices(listOf("日根野駅","泉佐野駅","熊取駅","鳳駅"))
+                    build()
+                }
+                logger.info { "Send screen event" }
+                build()
+            })
+        }
     }
 }
