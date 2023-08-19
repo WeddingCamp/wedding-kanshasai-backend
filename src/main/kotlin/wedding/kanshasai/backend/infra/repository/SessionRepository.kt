95c82212fc7ac4b4908c2247ad097e02c827c312
Ritsuki Goto <develop@mizucoffee.net>
2023-08-19 12:17:46 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/infra/repository/SessionRepository.kt
+++
@@ -1,9 +0,0 @@
-package wedding.kanshasai.backend.infra.repository
-
-import org.springframework.stereotype.Repository
-import wedding.kanshasai.backend.domain.value.UlidId
-
-@Repository
-class SessionRepository {
-    fun findById(ulid: UlidId) {}
-}
--- src/test/kotlin/wedding/kanshasai/backend/infra/mapper/SessionMapperTests.kt
+++ src/test/kotlin/wedding/kanshasai/backend/infra/mapper/SessionMapperTests.kt
@@ -3,27 +3,33 @@ package wedding.kanshasai.backend.infra.mapper
 import org.springframework.beans.factory.annotation.Autowired
 import wedding.kanshasai.backend.WeddingKanshasaiSpringBootTest
 import wedding.kanshasai.backend.domain.value.UlidId
+import wedding.kanshasai.backend.infra.dto.EventDto
 import wedding.kanshasai.backend.infra.dto.SessionDto
-import wedding.kanshasai.backend.infra.repository.EventRepository
 
 @WeddingKanshasaiSpringBootTest
 class SessionMapperTests : MapperCRUDTest<SessionMapper, SessionDto>() {
 
     @Autowired
-    lateinit var eventRepository: EventRepository
+    lateinit var eventMapper: EventMapper
 
     override fun prepareDto() {
         dtoList.addAll(
             (0..9).map {
-                val id = UlidId.new()
-                val event = eventRepository.createEvent("Event $it").getOrThrow()
+                val eventId = UlidId.new()
+                val eventDto = EventDto(
+                    eventId.toByteArray(),
+                    "Event_$eventId"
+                )
+                eventMapper.insert(eventDto)
+                val sessionId = UlidId.new()
                 SessionDto(
-                    id.toByteArray(),
-                    event.id.toByteArray(),
-                    "Session_$id",
+                    sessionId.toByteArray(),
+                    eventId.toByteArray(),
+                    "Session_$sessionId",
                     it,
                     100,
                     null,
+                    event = eventDto
                 )
             },
         )
