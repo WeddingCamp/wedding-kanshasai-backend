ffb5e46a27a36334da3b21cfd577100c73dc7cad
GOTO Ritsuki <develop@mizucoffee.net>
2023-10-09 14:07:46 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/controller/grpc/ParticipantController.kt
+++ src/main/kotlin/wedding/kanshasai/backend/controller/grpc/ParticipantController.kt
@@ -1,7 +1,6 @@
 package wedding.kanshasai.backend.controller.grpc
 
 import net.devh.boot.grpc.server.service.GrpcService
-import wedding.kanshasai.backend.controller.grpc.response.*
 import wedding.kanshasai.backend.domain.entity.Participant
 import wedding.kanshasai.backend.domain.exception.InvalidArgumentException
 import wedding.kanshasai.backend.domain.value.ParticipantType
