221c8dd9bab74f9dfa17fe1e9aede238066fb19b
GOTO Ritsuki <develop@mizucoffee.net>
2023-10-28 15:44:46 +0900

    [#220] Fix generate presigned url (#221)

--- src/main/kotlin/wedding/kanshasai/backend/controller/grpc/CommonController.kt
+++ src/main/kotlin/wedding/kanshasai/backend/controller/grpc/CommonController.kt
@@ -1,11 +1,11 @@
 package wedding.kanshasai.backend.controller.grpc
 
+import com.amazonaws.HttpMethod
 import net.devh.boot.grpc.server.service.GrpcService
 import wedding.kanshasai.backend.domain.value.UlidId
 import wedding.kanshasai.backend.service.S3Service
 import wedding.kanshasai.v1.*
 import wedding.kanshasai.v1.CommonServiceGrpcKt.CommonServiceCoroutineImplBase
-import java.util.*
 
 @GrpcService
 class CommonController(
@@ -13,7 +13,7 @@ class CommonController(
 ) : CommonServiceCoroutineImplBase() {
     override suspend fun generatePresignedUrl(request: GeneratePresignedUrlRequest): GeneratePresignedUrlResponse {
         val id = UlidId.new()
-        val url = s3Service.generatePresignedUrl(id)
+        val url = s3Service.generatePresignedUrl(id, HttpMethod.PUT)
         return GeneratePresignedUrlResponse.newBuilder()
             .setPresignedUrl(url)
             .setFileId(id.toString())
