4c97f526d757fd5ab1ba0a8100fa00ca241a8844
GOTO Ritsuki <develop@mizucoffee.net>
2023-11-03 10:58:45 +0900

    fix

--- src/main/kotlin/wedding/kanshasai/backend/service/QrCodeService.kt
+++ src/main/kotlin/wedding/kanshasai/backend/service/QrCodeService.kt
@@ -5,17 +5,16 @@ import io.github.g0dkar.qrcode.QRCode.Companion.DEFAULT_CELL_SIZE
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import wedding.kanshasai.backend.domain.entity.Session
-import wedding.kanshasai.backend.domain.value.UlidId
 
 @Service
 class QrCodeService(
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
 ) {
     @Value("\${frontend.url}")
     private val frontendUrl: String? = null
 
     fun generateQrCodeUrl(session: Session): String {
-        if(s3Service.isFileExists(session.id)) return s3Service.generatePresignedUrl(session.id)
+        if (s3Service.isFileExists(session.id)) return s3Service.generatePresignedUrl(session.id)
         QRCode("$frontendUrl/?s=${session.id}")
             .render(DEFAULT_CELL_SIZE, DEFAULT_CELL_SIZE)
             .getBytes()
